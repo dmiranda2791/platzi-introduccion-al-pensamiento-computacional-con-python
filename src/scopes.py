e2c42e8f944e24b7780fa8cb44b9499e2d013655
Daniel Miranda <dmiranda2791@gmail.com>
2020-06-05 20:28:56 -0600

    Scopes y docstrings

---
+++ src/scopes.py
@@ -0,0 +1,15 @@
+def func1(un_arg, una_func):
+  def func2(otro_arg):
+    return otro_arg * 2
+
+  valor = func2(un_arg)
+  return una_func(valor)
+
+un_arg = 1
+
+def cualquier_func(cualquier_arg):
+  return cualquier_arg + 5
+
+resultado = func1(un_arg, cualquier_func)
+
+print(f'El resultado es {resultado}')
