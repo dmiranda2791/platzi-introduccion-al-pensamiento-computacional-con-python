d18150a3d4c8dd828bbcfbdd9848a8cd8b0cb9bf
Daniel Miranda <dmiranda2791@gmail.com>
2020-07-03 20:02:16 -0600

    Probabilidad de obtener por 12 con dos dados

--- src/probabilidades/probabilidades.py
+++ src/probabilidades/probabilidades.py
@@ -4,8 +4,10 @@ def tirar_dado(numero_de_tiros):
   secuencia_de_tiros = []
 
   for _ in range(numero_de_tiros):
-    tiro = random.choice(list(range(1, 7)))
-    secuencia_de_tiros.append(tiro)
+    tiro_1 = random.choice(list(range(1, 7)))
+    tiro_2 = random.choice(list(range(1, 7)))
+    resultado = tiro_1 + tiro_2
+    secuencia_de_tiros.append(resultado)
   
   return secuencia_de_tiros
 
@@ -16,14 +18,14 @@ def main(numero_de_tiros, numero_de_intentos):
     secuencia_de_tiros = tirar_dado(numero_de_tiros)
     tiros.append(secuencia_de_tiros)
 
-  tiros_con_1 = 0
+  tiros_con_12 = 0
   for tiro in tiros:
-    if 1 not in tiro:
-      tiros_con_1 += 1 
+    if 12 in tiro:
+      tiros_con_12 += 1 
 
-  probabilidad_tiros_con_1 = tiros_con_1/len(tiros)
+  probabilidad_tiros_con_1 = tiros_con_12/len(tiros)
 
-  print(f'Probabilidad de no obtener por lo menos un 1 en {numero_de_tiros} es igual {probabilidad_tiros_con_1}')
+  print(f'Probabilidad de  obtener por lo menos un 12 en {numero_de_tiros} es igual {probabilidad_tiros_con_1}')
 if __name__ == '__main__':
   numero_de_tiros = int(input('Cuántos tiros del dado: '))
   numero_de_intentos = int(input('Cuántos intentos? '))
